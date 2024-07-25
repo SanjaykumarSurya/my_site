0f9964d42570a1db1f8669f777377ec89c112bce
sanjaykumar <sanjaykumarsurya76166@gmail.com>
2024-07-25 11:10:08 +0530

    finished project

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,8 @@ from .import views
 
 
 urlpatterns = [
-    path("", views.starting_page, name="starting-page"),
-    path("posts", views.posts, name="posts-page"),
-    path("posts/<slug:slug>", views.post_deatail, name="post-detail-page") #/posts/my-first-post # slug(dynamic path , eg:(<int:slug>),<str:slug>)
+    path("", views.StartingPageView.as_view(), name="starting-page"),
+    path("posts", views.AllPostView.as_view(), name="posts-page"),
+    path("posts/<slug:slug>", views.SinglePostView.as_view(), name="post-detail-page"), #/posts/my-first-post # slug(dynamic path , eg:(<int:slug>),<str:slug>)
+    path("read-later", views.ReadLaterView.as_view(), name="read-later")
 ]
